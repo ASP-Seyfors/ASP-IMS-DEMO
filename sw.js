72653ff01c2ab7388b9a67a0f30225c7c0755a77
ASP-Seyfors <thomas@alliedsurgicalproducts.com>
2026-09-23 20:35:42 -0400

    5.5.8 sync_engine addition and bug fixes for DB Editor, unnecessary Cloud Sync operations, and Reporting filenames.

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  * Copyright (c) 2026 Thomas Seyfors / Allied Surgical Products.
  * All Rights Reserved.
  * ======================================================================= */
-const CACHE_NAME = 'asp-ims-demo-v5.5.7';
+const CACHE_NAME = 'asp-ims-demo-v5.5.8';
 
 
 const ASSETS_TO_CACHE = [
